04a284e83f36fbecb2f46a2d9814f2bb5f404e1e
iaraechenique <iaraechenique0816@gmail.com>
2024-09-22 18:29:51 -0300

    funcionalidad

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,24 +1,56 @@
-let addbtn= document.getElementById("agregar");
-let input=document.getElementById("item");
-let list= document.getElementById("contenedor");
+const itemInput = document.getElementById('item');  
+const agregarButton = document.getElementById('agregar');  
+const contenedor = document.getElementById('contenedor'); 
+const limpiarButton = document.getElementById('limpiar'); 
 
-addbtn.addEventListener("click",function(){
 
-let newItem=document.createElement("li");
-
-if (input.value.trim()!= ""){
-    newItem.innerText=input.value;
-    list.appendChild(newItem);
-    
-//Guardamos el valor en el localStorage
-localStorage.setItem("inputDato", JSON.stringify(input.value));/*el valor que queremos agregar que es el del input.value
-                                                               no newItem que es la li en si.
-                                                               el método JSON.stringify()-> string, el local solo guarda texto*/
- console.log(localStorage)
-let retrieved=JSON.parse(localStorage.getItem("inputDato")) /*para revertir y obtener el valor guardado en su forma original
-                                                             JSON.parse()-> Obj*/
-console.log(retrieved)
+function obtenerItemsDeLocalStorage() {
+  const items = localStorage.getItem('items');  
+  return items ? JSON.parse(items) : [];  
 }
 
-});
+//  guardar en localStorage
+function guardarItemsEnLocalStorage(items) {
+  localStorage.setItem('items', JSON.stringify(items));  // Convierte el array en una cadena JSON y lo guarda en localStorage
+}
 
+// actualiza dom y arma lista
+function seaArmaLista() {
+  contenedor.innerHTML = '';  
+  const items = obtenerItemsDeLocalStorage();  
+
+  
+  items.forEach((item, index) => {
+    const li = document.createElement('li');
+    li.className = 'list-group-item';  
+    li.textContent = item;  
+
+    contenedor.appendChild(li);  
+  });
+}
+
+// Función para agregar un nuevo ítem
+function agregarItem() {
+  const nuevoItem = itemInput.value.trim();  
+
+  if (nuevoItem) {  
+    const items = obtenerItemsDeLocalStorage();  
+    items.push(nuevoItem);  
+    guardarItemsEnLocalStorage(items);  
+    seaArmaLista();  
+    itemInput.value = ''; 
+  }
+}
+
+// Función para limpiar la lista
+function limpiarLista() {
+  localStorage.removeItem('items');  
+  seaArmaLista(); 
+}
+
+// agrega eventos boton
+agregarButton.addEventListener('click', agregarItem);  
+limpiarButton.addEventListener('click', limpiarLista);  
+
+
+document.addEventListener('DOMContentLoaded', seaArmaLista);
